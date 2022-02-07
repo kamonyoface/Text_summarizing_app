db415c13493ed2dcc7238f609122cacd6bbd4b44
kamonyoface <56429221+kamonyoface@users.noreply.github.com>
2022-02-07 19:58:23 +0100

    Add files via upload
    ScreenManager with Navigation

---
+++ instruction.md
@@ -0,0 +1,1 @@
+# Example of a functioning app with Screen Manager
--- landing_page.py
+++ landing_page.py
@@ -3,15 +3,20 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager, Screen
 Window.size = (1400, 700)
+class Display(BoxLayout):
+    pass
+class MainScreen(Screen):
+    pass
 
-class MyLayout(BoxLayout):
+class OutputScreen(Screen):
     pass
 
 class landing_page(App):
     def build(self):
         Window.clearcolor = (1,1,1,1)
-        return MyLayout()
+        return Display()
 
 if __name__ == "__main__":
     window = landing_page()
