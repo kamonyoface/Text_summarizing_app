8d6f7b31eb21c2a02792c5d35570e24f33140c06
kamonyoface <56429221+kamonyoface@users.noreply.github.com>
2022-02-07 20:46:13 +0100

    Create landing_page.py

---
+++ ScreenManagerNavigationBar/landing_page.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.core.text import LabelBase
+from kivy.uix.screenmanager import ScreenManager, Screen
+Window.size = (1400, 700)
+class Display(BoxLayout):
+    pass
+class MainScreen(Screen):
+    pass
+
+class OutputScreen(Screen):
+    pass
+
+class landing_page(App):
+    def build(self):
+        Window.clearcolor = (1,1,1,1)
+        return Display()
+
+if __name__ == "__main__":
+    window = landing_page()
+    window.run()
